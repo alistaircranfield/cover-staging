071187cafae94ca44227b865946e2dee19b10d15
alistaircranfield <226908043+alistaircranfield@users.noreply.github.com>
2026-08-04 09:58:23 +0100

    Add files via upload

--- k.js
+++ k.js
@@ -1,1 +1,2 @@
+window.__POD_TEST = true;   // this is the staging copy
 window.__POD_KEYS={cr:"https://default9a12677ec2e94deba58aee1c59ac01.61.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/21/workflows/bb1e3f216eeb40ba830bd21f68580106/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=alHS-gharMluumeVMMzHi6CSaUehOXMboRcmvzyrUTg"};
